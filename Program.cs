f6b115ee7351a1d39e9264642c78049a30b8675a
Nikita Luksha <nikita.luksha@softensity.com>
2021-09-14 07:31:03 +0300

    Read data

--- Program.cs
+++ Program.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
 
 namespace AutoLogDataReader
 {
@@ -10,7 +12,28 @@ namespace AutoLogDataReader
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello");
+            Console.WriteLine("AutoLogDataReader \n");
+
+
+            using (SqlConnection connection = new SqlConnection())
+            {
+                connection.ConnectionString = @"Data Source =.; Initial Catalog = AutoLog; Integrated Security = True";
+                connection.Open();
+
+                string sql = "Select * From Inventory";
+                SqlCommand command = new SqlCommand(sql, connection);
+
+                using (SqlDataReader dataReader = command.ExecuteReader())
+                {
+                    while (dataReader.Read())
+                    {
+                        Console.WriteLine($"-> Make: {dataReader["Make"]}, PetName: {dataReader["PetName"]}, Color: {dataReader["Color"]}");
+                    }
+                }
+
+            }
+            
+            Console.WriteLine("\n Done");
             Console.ReadLine();
         }
     }
