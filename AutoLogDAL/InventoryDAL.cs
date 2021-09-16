58be33b1629ddfe1cbb0dcba99d98fe0cba65db0
Nikita Luksha <nikita.luksha@softensity.com>
2021-09-16 10:20:20 +0300

    Add DAL

---
+++ AutoLogDAL/InventoryDAL.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace AutoLogDAL
+{
+    public class InventoryDAL
+    {
+        private readonly string connectionString;
+        private SqlConnection sqlConnection = null;
+
+        public InventoryDAL() : this(@"Data Source=.; Integrated Security=true; Initial Catalog=AutoLog")
+        {
+        }
+
+        public InventoryDAL(string connectionString) => this.connectionString = connectionString;
+
+        private void OpenCOnnection()
+        {
+            sqlConnection = new SqlConnection { ConnectionString = connectionString };
+            sqlConnection.Open();
+        }
+
+        private void CloseConnection()
+        {
+            if(sqlConnection?.State != ConnectionState.Closed)
+            {
+                sqlConnection?.Close();
+            }
+        }
+    }
+}
