3c34712b6bebe57fcf77df979c3dec7b05b23e9a
Nikita Luksha <nikita.luksha@softensity.com>
2021-09-17 06:29:24 +0300

    Add CRUD methods

--- AutoLogDAL/InventoryDAL.cs
+++ AutoLogDAL/InventoryDAL.cs
@@ -5,6 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Data;
 using System.Data.SqlClient;
+using AutoLogDAL.Models;
 
 namespace AutoLogDAL
 {
@@ -19,7 +20,7 @@ namespace AutoLogDAL
 
         public InventoryDAL(string connectionString) => this.connectionString = connectionString;
 
-        private void OpenCOnnection()
+        private void OpenConnection()
         {
             sqlConnection = new SqlConnection { ConnectionString = connectionString };
             sqlConnection.Open();
@@ -27,10 +28,84 @@ namespace AutoLogDAL
 
         private void CloseConnection()
         {
-            if(sqlConnection?.State != ConnectionState.Closed)
+            if (sqlConnection?.State != ConnectionState.Closed)
             {
                 sqlConnection?.Close();
             }
+        }
+
+        public List<Car> GetAllInventory()
+        {
+            OpenConnection();
+
+            var inventory = new List<Car>();
+
+            string sql = "Select * From Inventory";
+
+            using (SqlCommand command = new SqlCommand(sql, sqlConnection))
+            {
+                command.CommandType = CommandType.Text;
+                SqlDataReader dataReader = command.ExecuteReader(CommandBehavior.CloseConnection);
+
+                while (dataReader.Read())
+                {
+                    inventory.Add(new Car
+                    {
+                        CarId = (int)dataReader["CarId"],
+                        Color = (string)dataReader["Color"],
+                        Make = (string)dataReader["Make"],
+                        PetName = (string)dataReader["PetName"]
+                    });
+                }
+
+                dataReader.Close();
+            }
+
+
+            return inventory;
+        }
+
+        public Car GetCar(int id)
+        {
+            OpenConnection();
+
+            Car car = null;
+            string sql = $"Select * From Inventory where CarId = {id}";
+
+            using (SqlCommand command = new SqlCommand(sql, sqlConnection))
+            {
+                command.CommandType = CommandType.Text;
+                SqlDataReader dataReader = command.ExecuteReader(CommandBehavior.CloseConnection);
+
+                while(dataReader.Read())
+                {
+                    car = new Car
+                    {
+                        CarId = (int)dataReader["CarId"],
+                        Color = (string)dataReader["Color"],
+                        Make = (string)dataReader["Make"],
+                        PetName = (string)dataReader["PetName"]
+                    };
+                }
+
+                dataReader.Close();
+            }
+
+            return car;
+        }
+
+        public void InsertAuto(string color, string make, string petName)
+        {
+            OpenConnection();
+
+            string sql = $"Insert Into Inventory (Make, Color, PetName) Values ('{make}', '{color}', '{petName}')";
+            using (SqlCommand command = new SqlCommand(sql, sqlConnection))
+            {
+                command.CommandType = CommandType.Text;
+                command.ExecuteNonQuery();
+            }
+
+            CloseConnection();
         }
     }
 }
